f14e2396ff7434b38a1107414b977a24b339b456
IKyllian <kyllian.delporte@gmail.com>
2024-10-27 22:16:41 +0100

    created multi range file

---
+++ front/src/components/input/multiRange.tsx
@@ -0,0 +1,13 @@
+import { multiRangeInputStyle } from "./multiRange.style"
+
+const MultiRangeInput = () => {
+  const slotsStyles = multiRangeInputStyle.raw()
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default MultiRangeInput
